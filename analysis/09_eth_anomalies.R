dea3930920481f3a5713f649fe43c8ee31771b29
Pauline Wairimu <paunimo@gmail.com>
2024-01-02 17:09:01 +0300

    adding code to combine all csv files into one

--- analysis/09_eth_anomalies.R
+++ analysis/09_eth_anomalies.R
@@ -65,3 +65,61 @@ plot_fxn(eth_shp, prec_file,
          subtitle = "Mid February to Mid May 2022", 
          mon_date = "2022-05-11",
          regions = c("Amhara", "Tigray"))
+
+### adding csv files
+eth_shp_adm3 <- st_read(
+    file.path(
+        Sys.getenv("AA_DATA_DIR"), 
+        "public", "raw", "eth", "cod_ab", 
+        "eth_adm_csa_bofedb_2021_shp", 
+        "eth_admbnda_adm3_csa_bofedb_2021.shp"))
+csv_path <- file.path(
+    Sys.getenv("AA_DATA_DIR"), 
+    "public", "exploration", "eth", "ndvi")
+
+afar_ssns <- c("mam", "jjas")
+am_tig_ssns <- c("fmam", "jjas")
+
+jjas2023_file <- read_csv(file.path(csv_path, "eth_ndvi_woreda_stats_jjas2023.csv"))
+colnames(jjas2023_file)[-1] = paste("JJAS 2023", colnames(jjas2023_file)[-1])
+jjas2022_file <- read_csv(file.path(csv_path, "eth_ndvi_woreda_stats_jjas2022.csv"))
+colnames(jjas2022_file)[-1] = paste("JJAS 2022", colnames(jjas2022_file)[-1])
+fmam2023_file <- read_csv(file.path(csv_path, "eth_ndvi_woreda_stats_fmam2023.csv"))
+colnames(fmam2023_file)[-1] = paste("FMAM 2023", colnames(fmam2023_file)[-1])
+fmam2022_file <- read_csv(file.path(csv_path, "eth_ndvi_woreda_stats_fmam2022.csv"))
+colnames(fmam2022_file)[-1] = paste("FMAM 2022", colnames(fmam2022_file)[-1])
+mam2023_file <- read_csv(file.path(csv_path, "eth_ndvi_woreda_stats_mam2023.csv"))
+colnames(mam2023_file)[-1] = paste("MAM 2023", colnames(mam2023_file)[-1])
+mam2022_file <- read_csv(file.path(csv_path, "eth_ndvi_woreda_stats_mam2022.csv"))
+colnames(mam2022_file)[-1] = paste("MAM 2022", colnames(mam2022_file)[-1])
+
+#put all data frames into list
+df_list <- list(jjas2023_file[,c(1,5,6,7)], 
+                jjas2022_file[,c(1,5,6,7)], 
+                fmam2023_file[,c(1,5,6,7)], 
+                fmam2022_file[,c(1,5,6,7)], 
+                mam2023_file[,c(1,5,6,7)], 
+                mam2022_file[,c(1,5,6,7)])      
+
+#merge all data frames together
+full_ndvi_df <- df_list %>% reduce(full_join, by="ADM3_PCODE")
+
+#rainfall df
+rain_dates <- c("2023-09-21", "2022-09-21", "2023-05-21", "2022-05-21",
+                "2023-09-01", "2022-09-01", "2023-05-11", "2022-05-11")
+
+full_rain_df <- prec_file %>%
+    filter(date %in% rain_dates) %>%
+    select(date, ADM2_PCODE, r3q) %>%
+    mutate(r3q = as.numeric(r3q)) %>%
+    pivot_wider(names_from = date, values_from = r3q, values_fn = {mean}) %>%
+    rename_with(~ paste0("3-month Rainfall Anomaly for Period Ending ", .x, recycle0 = TRUE),
+                starts_with("2"))
+
+full_df <- eth_shp_adm3 %>%
+    select(ADM3_PCODE, ADM3_EN, ADM2_PCODE) %>%
+    st_drop_geometry() %>%
+    merge(full_rain_df, by="ADM2_PCODE") %>%
+    merge(full_ndvi_df, by = "ADM3_PCODE")
+
+write_csv(full_df, file.path(csv_path, "full_seasonal_analysis.csv"))
